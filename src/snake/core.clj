c3b478cab3e38fd7b844bf2d9a8fb42bafbdb4f7
Yovan Naumovski <naumovski.jovan@gmail.com>
2016-09-22 21:33:28 +0300

    Move snake modulo board-size

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -4,8 +4,7 @@
 (defn zero-vector [size]
     (for [x (range size)] 0))
 
-(def board-h 10)
-(def board-w 10)
+(def board-size 10) ;; square board for simplicity
 
 (def left [-1 0])
 (def right [1 0])
@@ -13,28 +12,35 @@
 (def down [0 1])
 
 (def board 
-    (vec (for [x (range board-h)]
-        (into [] (zero-vector board-w)))))
+    (vec (for [x (range board-size)]
+          (into [] (zero-vector board-size)))))
 
 (defn print-board [board]
-    (dotimes [row board-h]
+    (dotimes [row board-size]
         (println (nth board row)))) 
 
+(defn +mod [x y]
+  (map #(mod (+ %1 %2) board-size) x y))
+
 (defn move-snake [snake direction]
-    (cons 
-        (into [] (map + (first snake) direction)) 
-        (pop snake)))
+ (cons 
+   (into [] (+mod (first snake) direction))
+   (pop snake)))
 
 (defn board-with-snake [board snake]
     (loop [piece 0
            b board]
         (if (>= piece (count snake))
-        b
-        (recur (inc piece) (assoc-in b (nth snake piece) "*")))))
+         b
+         (recur (inc piece) (assoc-in b (nth snake piece) "*")))))
     
+(defn board-with-apple [board apple]
+    (assoc-in board apple "X"))
+
 (defn -main
   [& args]
-  (let [snake [[4 4] [4 5] [4 6]]]
+  (let [snake [[0 0] [0 1] [0 2]]]
     ;(println (assoc-in board [1 1] "*"))
-    (print-board (board-with-snake board snake))
+    ;;(print-board (board-with-snake board snake))
+    ;;(print-board (board-with-apple board [2 2]))))
     (println (move-snake snake left))))
